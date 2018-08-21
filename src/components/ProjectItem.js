544fcc7afc25fb319fa63e2f860a4265dce54748
JustusFT <JustusFT@gmail.com>
2018-08-21 15:46:16 +0800

    Add source url to project list

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,8 +4,9 @@ const ProjectItem = ({ project }) => (
   <div>
     <h3>{project.title}</h3>
     <p>{project.description}</p>
-  <div><a href={project.url} target="_blank" rel="noopener noreferrer">View project</a></div>
-  <br /><hr />
+    <div><a href={project.url} target="_blank" rel="noopener noreferrer">View project</a></div>
+    <div><a href={project.sourceUrl} target="_blank" rel="noopener noreferrer">View source code</a></div>
+    <br /><hr />
   </div>
 )
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,43 +2,52 @@ export default [
   {
     title: 'Not Quora',
     description: 'A clone of the Q&A site Quora.',
-    url: 'https://whispering-plains-50835.herokuapp.com/'
-  },
-  {
+    url: 'https://whispering-plains-50835.herokuapp.com/',
+    sourceUrl: 'https://github.com/JustusFT/quora-clone-rails'
+  }, {
     title: 'Image Host',
-    description: 'A site where you can share images and create albums',
-    url: 'https://aqueous-caverns-77669.herokuapp.com/'
+    description: 'A site where you can upload images and create albums',
+    url: 'https://aqueous-caverns-77669.herokuapp.com/',
+    sourceUrl: 'https://github.com/JustusFT/image-host'
   }, {
     title: 'React Theremin',
     description: 'Move your mouse along the screen to produce sound. Based on the theremin, a musicical instrument.',
-    url: 'https://justusft.github.io/react-theremin'
+    url: 'https://justusft.github.io/react-theremin',
+    sourceUrl: 'https://github.com/JustusFT/react-theremin'
   }, {
     title: 'Alphabet Soup',
     description: 'Letters scattered around the screen arrange themselves to form messages.',
-    url: 'https://codepen.io/JustusFT/full/ENLZGJ'
+    url: 'https://codepen.io/JustusFT/full/ENLZGJ',
+    sourceUrl: 'https://codepen.io/JustusFT/pen/ENLZGJ'
   }, {
     title: 'Not Bitly',
     description: 'A url shortener based on bitly.com',
-    url: 'https://infinite-fortress-18426.herokuapp.com/'
+    url: 'https://infinite-fortress-18426.herokuapp.com/',
+    sourceUrl: 'https://github.com/JustusFT/bitly-clone'
   }, {
     title: 'Conway\'s Game of Life',
     description: 'Cellular automaton.',
-    url: 'https://codepen.io/JustusFT/full/MovwKJ/'
+    url: 'https://codepen.io/JustusFT/full/MovwKJ/',
+    sourceUrl: 'https://codepen.io/JustusFT/pen/MovwKJ/',
   }, {
     title: 'Tic tac toe',
     description: 'Featuring an unbeatable computer.',
-    url: 'https://codepen.io/JustusFT/full/jrrNVX/'
+    url: 'https://codepen.io/JustusFT/full/jrrNVX/',
+    sourceUrl: 'https://codepen.io/JustusFT/pen/jrrNVX/'
   }, {
     title: 'Pomodoro timer',
     description: 'A timer that follows the pomodoro technique.',
-    url: 'https://codepen.io/JustusFT/full/qNZKjq/'
+    url: 'https://codepen.io/JustusFT/full/qNZKjq/',
+    sourceUrl: 'https://codepen.io/JustusFT/pen/qNZKjq/'
   }, {
     title: 'Simon',
     description: 'Classic memory game.',
-    url: 'http://codepen.io/JustusFT/full/YWWrVZ'
+    url: 'http://codepen.io/JustusFT/full/YWWrVZ',
+    sourceUrl: 'http://codepen.io/JustusFT/pen/YWWrVZ'
   }, {
     title: 'Finger Battle',
     description: 'Two player vs game where the faster tapper wins.',
-    url: 'http://justusft.github.io/finger-battle'
+    url: 'http://justusft.github.io/finger-battle',
+    sourceUrl: 'https://github.com/JustusFT/finger-battle'
   }
 ]
