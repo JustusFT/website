c02fbb4abb54579f5d447d40b58bdcb1c8f69f4a
JustusFT <JustusFT@gmail.com>
2018-08-21 15:55:05 +0800

    Remove default theming

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import './index.css'
-
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
@@ -13,14 +11,7 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
