eb77b554972e4c92c84f8fdb380210f470138b1f
JustusFT <JustusFT@gmail.com>
2018-08-22 11:37:01 +0800

    Add contact to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const HeaderContainer = styled.div`
   height: 480px;
   background-color: #00bfff;
   margin: 1em 0;
+  position: relative;
 
   @media (max-width: 992px) {
     margin-top: 0;
@@ -13,17 +14,49 @@ const HeaderContainer = styled.div`
 `
 
 const HeaderText = styled.div`
+  background-color: white;
+  padding: 2em;
   align-self: center;
   text-align: center;
   margin: auto;
+`
+
+const Labels = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 0.5em;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-end;
+`
+
+const Label = styled.span`
+  display: inline-block;
+  flex: 0 0 auto;
+  margin-top: 0.5em;
+  padding: 0.25em 0.5em;
+  background-color: #ffffff;
+
+  > span {
+    margin-left: 0.75em;
+  }
 `
 
 const Header = () => (
   <HeaderContainer>
     <HeaderText>
       <h1>Justus Tumacder</h1>
-      <h4>Web Developer</h4>
+      <h3>Web Developer</h3>
     </HeaderText>
+    <Labels>
+      <Label>
+        <b>Email:</b><span><a href="mailto:justusft@gmail.com">JustusFT@gmail.com</a></span>
+      </Label>
+      <Label>
+        <b>Location:</b><span>Philippines</span>
+      </Label>
+    </Labels>
   </HeaderContainer>
 )
 
