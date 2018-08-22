9e0428e94caab17d4bfb4ef79eaccf6c396483fc
JustusFT <JustusFT@gmail.com>
2018-08-22 15:42:07 +0800

    Use graphql to query projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,11 +5,21 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-transformer-json',
     {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/util/typography',
       }
-    }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'data',
+        path: `${__dirname}/data/`,
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
   ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import PROJECTS from '../data/projects'
 import ProjectList from '../components/ProjectList'
 import Header from '../components/Header'
 
@@ -10,14 +9,32 @@ const Content = styled.div`
   padding: 1em;
 `
 
-const IndexPage = () => (
-  <div>
-    <Header />
-    <Content>
-      <h2>Web projects</h2>
-      <ProjectList projects={PROJECTS} />
-    </Content>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  const projects = data.allProjectsJson.edges.map(edge => edge.node)
+  return (
+    <div>
+      <Header />
+      <Content>
+        <h2>Web projects</h2>
+        <ProjectList projects={projects} />
+      </Content>
+    </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query GetAllProjects {
+    allProjectsJson {
+      edges {
+        node {
+          title
+          description
+          url
+          sourceUrl
+        }
+      }
+    }
+  }
+`
