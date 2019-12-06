102fe44104ff8e6c58030f4c6133feba0f62a9a2
Justus Tumacder <JustusFT@gmail.com>
2019-12-06 09:55:54 +0800

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,17 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-154135260-1',
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'data',
         path: `${__dirname}/data/`,
-      }
+      },
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
@@ -19,7 +25,7 @@ module.exports = {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/util/typography',
-      }
-    }
-  ]
+      },
+    },
+  ],
 }
